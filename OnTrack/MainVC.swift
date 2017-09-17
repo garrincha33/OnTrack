8d4531c5d472d379ff647332b912eb60fb44c2b1
Richard Price <mrspondooli@gmail.com>
2017-09-17 16:28:10 +0100

    added swipe to delete to mainVC

--- OnTrack/MainVC.swift
+++ OnTrack/MainVC.swift
@@ -17,17 +17,18 @@ class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-        
         tableView.delegate = self
         tableView.dataSource = self
         tableView.isHidden = false
-        
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        fetchGoals()
+        tableView.reloadData()
+    }
+    
+    func fetchGoals() {
         self.fetch { (complete) in
             if complete {
                 if goals.count >= 1 {
@@ -38,8 +39,6 @@ class MainVC: UIViewController {
                 }
             }
         }
-        
-        tableView.reloadData()
     }
     
     @IBAction func addGoralBtnPressed(_ sender: Any) {
@@ -49,58 +48,68 @@ class MainVC: UIViewController {
         }
         presentDetail(createGoalVC)
     }
-    
 }
 
 extension MainVC: UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        
         return 1
-    
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
         return goals.count
-        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "GoalCell") as? GoalCell else {
-            
-            return UITableViewCell() }
-        
+        return UITableViewCell() }
         let goal = goals[indexPath.row]
         cell.configureCell(goal: goal)
-
         return cell
-    
     }
+    
+    //----------------------- swipe to delete
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        return .delete
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let deleteAction = UITableViewRowAction(style: .destructive, title: "DELETE") { (rowAction, indexPath) in
+            self.deleteGoal(atindexPath: indexPath)
+            self.fetchGoals()
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+        deleteAction.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        return [deleteAction]
+    }
+     //----------------------- swipe to delete
 }
 
-
 extension MainVC {
+    func deleteGoal(atindexPath indexPath: IndexPath) {
+        guard let managedContext = context else {return}
+        managedContext.delete(goals[indexPath.row])
+        do {
+            try managedContext.save()
+        } catch {
+            print("error in saving : \(error.localizedDescription)")
+        }
+    }
     
     func fetch(completion: (_ Complete: Bool) -> ()) {
-        
         guard let managedContext = context else {return}
         let fetchRequest = NSFetchRequest<Goal>(entityName: "Goal")
-        
         do {
-            
             goals = try managedContext.fetch(fetchRequest)
             print("successfully loaded from coredata")
             completion(true)
-            
         } catch {
-            
             print("could not fetch : \(error.localizedDescription)")
             completion(false)
         }
     }
 }
-
-
-
